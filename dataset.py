84cb9f0e07e40431e1047cebb44b28e29540864e
LucaColamarino <colamarino01@gmail.com>
2025-07-09 10:55:34 +0200

    model checkpopints

--- dataset.py
+++ dataset.py
@@ -5,33 +5,28 @@ from torch.utils.data import Dataset
 import numpy as np
 
 PALETTE2ID = {
-    (128,  64, 128): 0,   # road
-    (244,  35, 232): 1,   # sidewalk
-    ( 70,  70,  70): 2,   # building
-    (102, 102, 156): 3,   # wall
-    (190, 153, 153): 4,   # fence
-    (153, 153, 153): 5,   # pole
-    (250, 170,  30): 6,   # traffic light
-    (220, 220,   0): 7,   # traffic sign
-    (107, 142,  35): 8,   # vegetation
-    (152, 251, 152): 9,   # terrain
-    ( 70, 130, 180): 10,  # sky
-    (220,  20,  60): 11,  # person
-    (255,   0,   0): 12,  # rider
-    (  0,   0, 142): 13,  # car
-    (  0,   0,  70): 14,  # truck
-    (  0,  60, 100): 15,  # bus
-    (  0,  80, 100): 16,  # train
-    (  0,   0, 230): 17,  # motorcycle
-    (119,  11,  32): 18,  # bicycle
+    (128,  64, 128): 0, (244,  35, 232): 1, ( 70,  70,  70): 2,
+    (102, 102, 156): 3, (190, 153, 153): 4, (153, 153, 153): 5,
+    (250, 170,  30): 6, (220, 220,   0): 7, (107, 142,  35): 8,
+    (152, 251, 152): 9, ( 70, 130, 180): 10, (220,  20,  60): 11,
+    (255,   0,   0): 12, (  0,   0, 142): 13, (  0,   0,  70): 14,
+    (  0,  60, 100): 15, (  0,  80, 100): 16, (  0,   0, 230): 17,
+    (119,  11,  32): 18,
 }
+
+COLORS = np.array([
+    (128,  64,128), (244,  35,232), ( 70,  70, 70), (102,102,156), (190,153,153),
+    (153,153,153), (250,170, 30), (220,220,  0), (107,142, 35), (152,251,152),
+    ( 70,130,180), (220, 20, 60), (255,  0,  0), (  0,  0,142), (  0,  0, 70),
+    (  0, 60,100), (  0, 80,100), (  0,  0,230), (119, 11, 32)
+], dtype=np.uint8)
+
 
 def rgb_to_id(mask_rgb, palette2id=PALETTE2ID, default=255):
     h, w, _ = mask_rgb.shape
     mask_id = np.full((h, w), default, dtype=np.uint8)
     for rgb, cls_id in palette2id.items():
-        matches = (mask_rgb == rgb).all(axis=-1)
-        mask_id[matches] = cls_id
+        mask_id[(mask_rgb == rgb).all(axis=-1)] = cls_id
     return mask_id
 
 class CityscapesDataset(Dataset):
@@ -45,19 +40,15 @@ class CityscapesDataset(Dataset):
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.files[idx])
         label_path = os.path.join(self.label_dir, self.files[idx])
-        image = Image.open(img_path).convert('RGB')
-        label_rgb = np.array(Image.open(label_path))
-        label_id = rgb_to_id(label_rgb)
-        label = Image.fromarray(label_id)
 
-        # Resize both
-        image = image.resize(self.resize, Image.BILINEAR)
-        label = label.resize(self.resize, Image.NEAREST)
+        image = Image.open(img_path).convert('RGB').resize(self.resize, Image.BILINEAR)
+        label_rgb = Image.open(label_path).resize(self.resize, Image.NEAREST)
+        label_id = rgb_to_id(np.array(label_rgb))
 
         if self.transform:
             image = self.transform(image)
 
-        return image, torch.from_numpy(np.array(label)).long()
+        return image, torch.from_numpy(label_id).long()
 
     def __len__(self):
         return len(self.files)
