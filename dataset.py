05c0c0e6ca1a970da73b3bb2520f4a974a4e7fa7
LucaColamarino <colamarino01@gmail.com>
2025-07-18 01:03:36 +0200

    colors

--- dataset.py
+++ dataset.py
@@ -28,18 +28,13 @@ CITYSCAPES_19_TO_7 = {
 }
 DEFAULT_BG_CLASS = 6  # Usalo per i pixel non mappati
 
-
-
-
-
-
-
 COLORS = np.array([
     (128,  64,128), (244,  35,232), ( 70,  70, 70), (102,102,156), (190,153,153),
     (153,153,153), (250,170, 30), (220,220,  0), (107,142, 35), (152,251,152),
     ( 70,130,180), (220, 20, 60), (255,  0,  0), (  0,  0,142), (  0,  0, 70),
     (  0, 60,100), (  0, 80,100), (  0,  0,230), (119, 11, 32), (255, 255, 255) # Add white for unknown obstacles
 ], dtype=np.uint8)
+
 
 # Official Dataset with gtFine
 class CityscapesFineDataset(Dataset):
